0d8877cb7a5f9cb421b3e4c1f5b3c5c7e9a9f1f0
Kai-Chun Yang <kelly21225@gmail.com>
2022-04-04 21:53:22 +0800

    Update: Add home and other _project pages.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,54 @@
-$(function(){
-    $(".headerPage").load("../layout/header.html");
-    $(".footerPage").load("../layout/footer.html");
+$(document).ready(function () {
+    $(".header_page").load("../layout/header.html");
+    $(".footer_page").load("../layout/footer.html");
+
+    // top
+    $(".top").click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // more_detail hover
+    $(".more_detail").hover(function () {
+        $(this).find("p").animate({ bottom: "0px" });
+    }, function () {
+        $(this).find("p").animate({ bottom: "-60px" });
+    });
+
+    // more_detail click
+    $(".more_detail").click(function (e) {
+        e.preventDefault();
+        $(this).siblings(".modal").fadeIn(600);
+    });
+
+    // close
+    $(".close").click(function (e) {
+        e.preventDefault();
+        $(this).parent().fadeOut(600);
+    });
+
+    // swiper
+    const swiper = new Swiper(".swiper", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+        speed: 2000,
+        autoplay: {
+            delay: 2000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    });
 });
